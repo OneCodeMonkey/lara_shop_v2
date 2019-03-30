79f04c3ece386243588226a4b46cc5fa67bd7983
OneCodeMonkey <1460018362@qq.com>
2019-03-30 17:43:29 +0000

    app\Events

---
+++ app/Events/OrderPaid.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PricateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class OrderPaid
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        $this->order=  $order;
+    }
+
+    /**
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
