495ed400bcac60efb152ad0adf8d3375800f92af
OneCodeMonkey <1460018362@qq.com>
2019-03-31 17:31:55 +0000

    add controller:coupon codes controller

---
+++ app/Http/Controllers/CouponCodesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\CouponCodeUnavailableException;
+use App\Models\CouponCode;
+use Illuminate\Http\Request;
+
+class CouponCodesController extends Controller
+{
+    public function show($code, Request $request)
+    {
+        if (!$record = CouponCode::where('code', $code)->first()) {
+            throw new CouponCodeUnavailableException('优惠券不存在');
+        }
+        $record->checkAvailable($request->user());
+
+        return $record;
+    }
+}
