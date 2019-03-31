d09bc4ed8fbf0b0a196fea1b582de892fb4d5446
OneCodeMonkey <1460018362@qq.com>
2019-03-31 17:28:06 +0000

    add controller:cart controller

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,3 +7,34 @@ use App\Http\Requests\AddCartRequest;
 use App\Models\ProductSku;
 use App\Services\CartService;
 
+class CartController extends Controller
+{
+    protected $cartService;
+
+    // 利用Laravel的自动解析功能，注入CartService类
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $this->cartService->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
+        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
+    }
+
+    public function add(AddCartRequest $request)
+    {
+        $this->cartService->add($request->input('sku_id'), $request->input('amount'));
+
+        return [];
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $this->cartService->remove($sku->id);
+
+        return [];
+    }
+}
