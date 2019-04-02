2c9e1f3f8ce88620feed61b5c3613f89e7e49c59
OneCodeMonkey <1460018362@qq.com>
2019-04-02 08:32:45 +0000

    add service: order service

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -46,8 +46,191 @@ class OrderService
             $totalAmount = 0;
 
             foreach ($items as $data) {
-                $sku = 
+                $sku = ProductSku::find($data['sku_id']);
+                // 创建一个 OrderItem 并直接与当前订单关联
+                $item = $order->items()->make([
+                    'amount' => $data['amount'],
+                    'price' => $sku->price,
+                ]);
+                $item->product()->asscciate($sku->product_id);
+                $item->productSku()->associate($sku);
+                $item->save();
+
+                $totalAmount += $sku->price * $data['amount'];
+                if ($sku->decreaseStock($data['amount']) <= 0) {
+                    throw new InvalidRequestException('该商品库存不足');
+                }
             }
-        })
+            if ($coupon) {
+                // 总金额已经计算拿出来了，检查是否符合优惠券的规则
+                $coupon->checkAvailable($user, $totalAmount);
+                // 把订单金额修改为优惠后的金额
+                $totalAmount = $coupon->getAdjustedPrice($totalAmount);
+                // 将订单与优惠券关联
+                $order->couponCode()->associate($coupon);
+                // 增加优惠券的用量，需判断返回值
+                if ($coupon->changeUsed() <= 0) {
+                    throw new CouponCodeUnavailableException('该优惠券已被兑完');
+                }
+            }
+            // 更新订单总金额
+            $order->update(['total_amount' => $totalAmount]);
+            // 将下单的商品从购物车中移除
+            $skuIds = collect($items)->pluck('sku_id')->all();
+            app(CartService::class)->remove($skuIds);
+
+            return $order;
+        });
+
+        // 这里我们直接用 dispatch 函数
+        dispatch(new CloseOrder($order, config('app.order_ttl')));
+
+        return $order;
+    }
+
+    // 新建一个 crowdfunding() 用于实现众筹商品下单的逻辑
+    public function crowdfunding(User $user, UserAddress $address, ProductSku $sku, $amount)
+    {
+        // 开启事务
+        $order = \DB::transaction(function () use ($amount, $sku, $user, $address) {
+            // 更新地址最后使用时间
+            $address->update(['last_used_at' => Carbon::now()]);
+
+            // 创建一个订单
+            $order = new Order([
+                'address' => [
+                    'address' => $address->full_address,
+                    'zip' => $address->zip,
+                    'contact_name' => $address->contact_name,
+                    'contact_phone' => $address->contact_phone,
+                ],
+                'remark' => '',
+                'total_amount' => $sku->price * $amount,
+                'type' => Order::TYPE_CROWDFUNDING,
+            ]);
+            // 将订单关联到当前用户
+            $order->user()->associate($user);
+            // 写入数据库
+            $order->save();
+            // 创建一个新的订单项并与 SKU 关联
+            $item = $order->items()->make([
+                'amount' => $amount,
+                'price' => $sku->price,
+            ]);
+            $item->product()->associate($sku->product_id);
+            $item->productSku()->associate($sku);
+            $item->save();
+            // 扣减对应的 SKU 库存
+            if ($sku->decreaseStock($amount) <= 0) {
+                throw new InvalidRequestException('该商品库存不足');
+            }
+
+            return $order;
+        });
+
+        // 众筹结束时间 - 当前时间 = 剩余秒数
+        $crowdfundingTtl = $sku->product->crowdfunding->end_at->getTimestamp() - time();
+        // 剩余秒数与默认订单关闭时间取较小值作为订单关闭时间
+        dispatch(new CloseOrder($order, min(config('app.order_ttl'), $crowdfundingTtl)));
+
+        return $order;
+    }
+
+    public function seckill(User $user, array $addressData, ProductSku $sku)
+    {
+        $order = \DB::transaction(function () use ($user, $addressData, $sku) {
+            // 创建一个订单
+            $order = new Order([
+                'address' => [
+                    'address' => $addressData['province'] . $addressData['city'] . $addressData['district'] . $addressData['address'],
+                    'zip' => $addressData['zip'],
+                    'contact_name' => $addressData['contact_name'],
+                    'contact_phone' => $addressData['contact_phone'],
+                ],
+                'remark' => '',
+                'total_amount' => $sku->price,
+                'type' => Order::TYPE_SECKILL,
+            ]);
+
+            // 将订单关联到当前用户
+            $order->user()->associate($user);
+            // 写入数据库
+            $order->save();
+            // 创建一个新的订单项并与 SKU 关联
+            $item = $order->items()->make([
+                'amount' => 1, // 秒杀商品只限一份
+                'price' => $sku->price,
+            ]);
+            $item->product()->associate($sku->product_id);
+            $item->productSku()->associate($sku);
+            $item->save();
+
+            // 扣减对应 SKU 库存
+            if ($sku->decreaseStock(1) <= 0) {
+                throw new InvalidRequestException('该商品库存不足');
+            }
+            \Redis::decr('seckill_sku_' . $sku->id);
+
+            return $order;
+        });
+        // 秒杀订单的自动关闭时间与普通订单不同
+        dispatch(new CloseOrder($order, config('app.seckill_order_ttl')));
+
+        return $order;
+    }
+
+    public function refundOrder(Order $order)
+    {
+        // 判断该订单的支付方式
+        switch ($order->payment_method) {
+            case 'wechat':
+                // 生成退款订单号
+                $refundNo = Order::getAvailableRefundNo();
+                app('wechat_pay')->refund([
+                    'out_trade_no' => $order->no,
+                    'total_fee' => $order->total_amount * 100,
+                    'refund_fee' => $order->total_amount * 100,
+                    'out_refund_no' => $refundNo,
+                    'notify_url' => ngrok_url('payment.wechat.refund_notify'),
+                ]);
+                $order->update([
+                    'refund_no' => $refundNo,
+                    'refund_status' => Order::REFUND_STATUS_PROCESSING,
+                ]);
+                break;
+            case 'alipay':
+                $refundNo = Order::getAvailableRefundNo();
+                $ret = app('alipay')->refund([
+                    'out_trade_no' => $order->no,
+                    'refund_amount' => $order->total_amount,
+                    'out_request_no' => $refundNo,
+                ]);
+                if ($ret->sub_code) {
+                    $extra = $order->extra;
+                    $extra['refund_failed_code'] = $ret->sub_code;
+                    $order->update([
+                        'refund_no' => $refundNo,
+                        'refund_status' => Order::REFUND_STATUS_FAILED,
+                        'extra' => $extra,
+                    ]);
+                } else {
+                    $order->update([
+                        'refund_no' => $refundNo,
+                        'refund_status' => Order::REFUND_STATUS_SUCCESS,
+                    ]);
+                }
+                break;
+            case 'installment':
+                $order->update([
+                    'refund_no' => Order::getAvailableRefundNo(), // 生成退款订单号
+                    'refund_status' => Order::REFUND_STATUS_PROCESSING, // 将退款状态改为退款中
+                ]);
+                // 触发退款异步任务
+                dispatch(new RefundInstallmentOrder($order));
+                break;
+            default:
+                throw new InternalException('未知的订单支付方式：' . $order->payment_method);
+                break;
+        }
     }
 }
