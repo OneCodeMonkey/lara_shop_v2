98dbea7cbc07d32cf725bb918e6358e7ec4fa95d
OneCodeMonkey <1460018362@qq.com>
2019-04-02 16:49:39 +0000

    add controller: payment controller

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -96,6 +96,110 @@ class PaymentController extends Controller
         $order = Order::where('no', $data->out_trade_no)->first();
         // 订单不存在则告知微信支付
         if (!$order) return 'fail';
-        
+        // 订单已支付
+        if ($order->paid_at) {
+            // 告知微信支付此订单已处理
+            return app('wechat_pay')->success();
+        }
+        // 将订单标记为已支付
+        $order->update([
+            'paid_at' => Carbon::now(),
+            'payment_method' => 'wechat',
+            'payment_no' => $data->transaction_id,
+        ]);
+        $this->afterPaid($order);
+
+        return app('wechat_pay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
+    }
+
+    public function wechatRefundNotify(Request $request)
+    {
+        // 给微信的失败响应
+        $failXml = '<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[FAIL]]></return_msg></xml>';
+        $data = app('wechat_pay')->verify(null, true);
+
+        // 没有找到对应的订单，原则上不可能发生，但还是要保证代码健壮性
+        if (!$order = Order::where('no', $data['out_trade_no'])->first()) {
+            return $failXml;
+        }
+
+        if ($data['refund_status'] === 'SUCCESS') {
+            // 退款成功，将订单退款状态改为退款成功
+            $order->update(['refund_status' => Order::REFUND_STATUS_SUCCESS]);
+        } else {
+            // 退款失败，将具体状态存入 extra 字段，并把退款状态改为失败
+            $extra = $order->extra;
+            $extra['refund_failed_code'] = $data['refund_status'];
+            $order->update([
+                'refund_status' => Order::REFUND_STATUS_FAILED,
+                'extra' => $extra,
+            ]);
+        }
+
+        return app('wechat_pay')->success();
+    }
+
+    public function payByInstallment(Order $order, Request $request)
+    {
+        // 判断订单是否属于当前用户
+        $this->authorize('own', $order);
+        // 订单已支付或已关闭
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 校验用户提交的还款月数，数值必须是我们配置好费率的期数
+        $this->validate($request, [
+            'count' => ['required', Rule::in(array_keys(config('app.installment_fee_rate')))],
+        ]);
+        // 删除同一笔商品订单发起过其他的状态是未支付的分期付款，避免同一笔商品订单有多个分期付款
+        Installment::query()
+            ->where('order_id', $order->id)
+            ->where('status', Installment::STATUS_PENDING)
+            ->delete();
+        $count = $request->input('count');
+        // 创建一个新的分期付款对象
+        $installment = new Installment([
+            // 总本金即为商品订单总金额
+            'total_amount' => $order->total_amount,
+            // 分期期数
+            'count' => $count,
+            // 从配置文件中读取相应期数的费率
+            'fee_rate' => config('app.installment_fee_rate')[$count],
+            // 从配置文件中读取当前逾期费率
+            'fine_rate' => config('app.installment_fine_rate'),
+        ]);
+        $installment->user()->associate($request->user());
+        $installment->order()->associate($order);
+        $installment->save();
+
+        // 第一期的还款截止日期为明天凌晨0点
+        $dueDate = Carbon::tomorrow();
+        // 计算每一期的本金
+        $base = big_number($order->total_amount)->divide($count)->getValue();
+        // 计算每一期的手续费
+        $fee = big_number($base)->multiply($installment->fee_rate)->divide(100)->getValue();
+        // 根据用户选择的还款期数，创建对应数量的还款计划
+        for ($i = 0; $i < $count; $i++) {
+            // 最后一期的本金需要用总本金减去前面几期的本金
+            if ($i === $count - 1) {
+                $base = big_number($order->total_amount)->subtract(big_number($base)->multiply($count - 1));
+            }
+            $installment->items()->create([
+                'sequence' => $i,
+                'base' => $base,
+                'fee' => $fee,
+                'due_date' => $dueDate,
+            ]);
+
+            // 还款截止日期加 30 天
+            $dueDate = $dueDate->copy()->addDays(30);
+        }
+
+        return $installment;
     }
 }
