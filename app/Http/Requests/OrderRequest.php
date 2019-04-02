7c1820c832e607c9447181e1064317c394e0f8ac
OneCodeMonkey <1460018362@qq.com>
2019-04-02 09:45:15 +0000

    add Http\Request: order request

---
+++ app/Http/Requests/OrderRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use App\Models\ProductSku;
+
+class OrderRequest extends Request
+{
+    public function rules()
+    {
+        return [
+            // 判断当前用户提交的地址 ID 是否存在数据表中，并且是否属于当前用户
+            // 后面这个条件非常重要，否则恶意的用户可以用不同的地址 ID 不断地提交订单，来遍历输出平台所有用户的收获地址
+            'address_id' => [
+                'required',
+                Rule::exists('user_addresses', 'id')->where('user_id', $this->user()->id),
+            ],
+            'items' => ['required', 'array'],
+            'items.*.sku_id' => [
+                // 检查 items 数组下每一个子数组的 sku_id 参数
+                'required',
+                function ($attribute, $value, $fail) {
+                    if (!$sku = ProductSku::find($value)) {
+                        return $fail('该商品不存在');
+                    }
+                    if (!$sku->product->on_sale) {
+                        return $fail('该商品未上架');
+                    }
+                    if ($sku->stock === 0) {
+                        return $fail('该商品已售完');
+                    }
+                    // 获取当前索引
+                    preg_match('/items\.(\d+)\.sku_id/', $attribute, $m);
+                    $index = $m[1];
+                    // 根据索引找到用户所提交的购买数量
+                    $amount = $this->input('items')[$index]['amount'];
+                    if ($amount > 0 && $amount > $sku->stock) {
+                        return $fail('该商品库存不足');
+                    }
+                },
+            ],
+            'items.*.amount' => ['required', 'integer', 'min:1'],
+        ];
+    }
+}
