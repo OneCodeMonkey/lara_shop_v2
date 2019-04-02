41dcdd3caae927cf7db2d39c70255e34ab9278b5
OneCodeMonkey <1460018362@qq.com>
2019-04-02 10:00:25 +0000

    add Http\Request: seckill product order request

---
+++ app/Http/Requests/SeckillOrderRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductSku;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\Rules;
+
+class SeckillOrderRequest extends Request
+{
+    public function rules()
+    {
+        return [
+            'address.province' => 'required',
+            'address.city' => 'required',
+            'address.distinct' => 'required',
+            'address.address' => 'required',
+            'address.zip' => 'required',
+            'address.contact_name' => 'required',
+            'address.contact_phone' => 'required',
+            'sku_id' => [
+                'required',
+                function ($attribute, $value, $fail) {
+                    // 从 Redis 中读数据
+                    $stock = \Redis::get('seckill_sku_' . $value);
+                    // 如果是null，代表这个 SKU 不是秒杀商品
+                    if (is_null($stock)) return $fail('该商品不存在');
+                    // 判断库存
+                    if ($stock < 1) return $fail('该商品已售完');
+
+                    // 大多数用户在上面的逻辑里已经被拒绝了，因此下方的 SQL 查询不会对整体性能有太大影响
+                    $sku = ProductSku::find($value);
+                    if ($sku->product->seckill->is_before_start) return $fail('秒杀尚未开始');
+
+                    if ($sku->product->seckill->is_after_end) return $fail('秒杀已经结束');
+
+                    if (!$user = \Auth::user()) {
+                        throw new AuthenticationException('请先登录');
+                    }
+                    if (!$user->email_verified) {
+                        throw new InvalidRequestException('请先验证邮箱');
+                    }
+                    if ($order = Order::query()
+                        ->where('user_id', $this->user()->id)
+                        ->whereHas('items', function ($query) use ($value) {
+                            // 筛选出包含当前 SKU 的订单
+                            $query->where('product_sku_id', $value);
+                        })
+                        ->where(function ($query) {
+                            // 已支付的订单
+                            $query->whereNotNull('paid_at')
+                                ->orWhere('closed', false);
+                        })
+                        ->first()) {
+                        if ($order->paid_at) {
+                            return $fail('你已经抢购了该商品');
+                        }
+
+                        return $fail('你已经下单了该商品，请到订单页面支付');
+                    }
+                },
+            ],
+        ];
+    }
+}
