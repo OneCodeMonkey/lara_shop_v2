57c659265e8731506cb57a5963a09bc1046a039f
OneCodeMonkey <1460018362@qq.com>
2019-04-02 12:12:23 +0000

    [editing]add controller: payment controller

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\OrderPaid;
+use App\Exceptions\InvalidRequestException;
+use App\Models\Installment;
+use App\Models\Order;
+use Carbon\Carbon;
+use Endroid\QrCode\QrCode;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class PaymentController extends Controller
+{
+    public function payByAlipay(Order $order, Request $request)
+    {
+        // 判断订单是否属于当前用户
+        $this->authorize('own', $order);
+
+        // 订单已支付或已关闭
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 调用支付宝的网页支付
+        return app('alipay')->web([
+            'out_trade_no' => $order->no, // 订单编号，需保证在商户端不重复
+            'total_amount' => $order->total_amount, // 订单金额，单位(元), 支持小数点后两位
+            'subject' => '支付 Laravel Shop 的订单：' . $order->no, // 订单标题
+        ]);
+    }
+
+    public function alipayReturn()
+    {
+        try {
+            app('alipay')->verify();
+        } catch (\Exception $e) {
+            return view('pages.error', ['msg' => '数据不正确']);
+        }
+
+        return view('pages.success', ['msg' => '付款成功']);
+    }
+
+    public function alipayNotify()
+    {
+        // 校验输入参数
+        $data = app('alipay')->verify();
+        // 如果订单状态不是成功或者结束，则不走后续的逻辑
+        // 所有校验状态：https://docs.open.alipay.com/59/103672
+        if (!in_array($data->trade_status, ['TRADE_SUCCESS', 'TRADE_FINISHED'])) {
+            return app('alipay')->success();
+        }
+        // $data->out_trade_no 拿到订单流水号，并在数据库中查询
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 正常来说不太可能支付了一笔不存在的订单，下面这个判断是为了系统健壮性
+        if (!$order) return 'fail';
+        // 如果这笔订单的状态是【已支付】
+        if ($order->paid_at) {
+            // 返回数据给支付宝
+            return app('alipay')->success();
+        }
+
+        $order->update([
+            'paid_at' => Carbon::now(), // 支付时间
+            'payment_method' => 'alipay', // 支付方式
+            'payment_no' => $data->trade_no, // 支付宝订单号
+        ]);
+        $this->afterPaid($order);
+
+        return app('alipay')->success();
+    }
+
+    public function payByWechat(Order $order, Request $request)
+    {
+        $this->authorize('own', $order);
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+        // 之前是直接返回，现在是把返回值放入一个变量里
+        $wechatOrder = app('wechat_pay')->scan([
+            'out_trade_no' => $order->no,
+            'total_fee' => $order->total_amount * 100,
+            'body' => '支付 Laravel Shop 的订单：' . $order->no,
+        ]);
+        // 把要转换的字符串作为 QrCode 的构造函数参数
+        $qrCode = new QrCode($wechatOrder->code_url);
+        // 将生成的二维码图片数据，以字符串形式输出，并带上相应的响应类型
+        return response($qrCode->writeString(), 200, ['Content-Type' => $qrCode->getContentType()]);
+    }
+
+    public function wechatNotify()
+    {
+        // 校验回调参数是否正确
+        $data = app('wechat_pay')->verify();
+        // 找到对应的订单
+        $order = Order::where('no', $data->out_trade_no)->first();
+        // 订单不存在则告知微信支付
+        if (!$order) return 'fail';
+        
+    }
+}
