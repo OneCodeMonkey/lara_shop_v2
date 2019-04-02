33d7a5a1299c24c37b864be5bda71ca9a93dbdce
OneCodeMonkey <1460018362@qq.com>
2019-04-02 18:46:15 +0000

    [editing]add controller: products controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidRequestException;
+use App\Models\Category;
+use App\Models\OrderItem;
+use App\Models\Product;
+use App\SearchBuilders\ProductSearchBuilder;
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class ProductsController extends Controller
+{
+    public function index(Request $request)
+    {
+        $page = $request->input('page', 1);
+        $perPage = 16;
+
+        // 新建一个查询构造器，设置只搜索上架的商品，设置分页
+        $builder = (new ProductSearchBuilder())->onSale()->paginate($perPage, $page);
+
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 调用查询构造器的类目筛选
+            $builder->category($category);
+        }
+
+        if ($search = $request->input('search', '')) {
+            $keywords = array_filter(explode(' ', $search));
+            // 调用查询构造器的关键词筛选
+            $builder->keywords($keywords);
+        }
+
+        if ($search || isset($category)) {
+            // 查询构造器的分面搜索
+            $builder->aggregateProperties();
+        }
+
+        $propertyFilters = [];
+
+        if ($filterString = $request->input('filters')) {
+            $filterArray = exploed('|', $filterString);
+            foreach ($filterArray as $filter) {
+                list($name, $value) = explode(':', $filter);
+                $propertyFilters[$name] = $value;
+
+                // 查询构造器的属性筛选
+                $builder->propertyFilter($name, $value);
+            }
+        }
+
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 查询构造器的排序
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        // 最后通过 getParams() 方法取回构造好的查询参数
+        $result = app('es')->search($builder->getParams());
+        $properties = [];
+
+        // 如果返回结果里有 aggregations 字段，则说明已经做了分面搜索
+        if (isset($result['aggregations'])) {
+            // 使用 collect 函数将返回值转为集合
+            $properties = collect($result['aggregations']['properties']['properties']['buckets'])
+                ->map(function ($bucket) {
+                    // 通过 map() 取出我们需要的字段
+                    return [
+                        'key' => $bucket['key'],
+                        'values' => collect($bucket['value']['buckets'])->pluck('key')->all(),
+                    ];
+                })
+                ->filter(function ($property) use ($propertyFilters) {
+                    // 过滤掉只剩下一个值或已经在筛选条件里的属性
+                    return count($property['values']) > 1 && !isset($propertyFilters[$property['key']]);
+                });
+        }
+
+        // 通过 collect() 将返回结果转为集合，并通过集合的 pluck() 取到返回的商品 ID 数组
+        $productIds = collect
+    }
+}
