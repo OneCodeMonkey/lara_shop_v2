3cd337d8d8c17282b97449f26ec316f7f705f429
OneCodeMonkey <1460018362@qq.com>
2019-04-03 08:58:21 +0000

    add job: close order

---
+++ app/Jobs/CloseOrder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Models\Order;
+
+// 代表这个类需要被放到队列中执行，而不是触发时立刻执行
+class CloseOrder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $order;
+
+    public function __construct(Order $order, $delay)
+    {
+        $this->order = $order;
+        // 设置延迟的时间，delay() 的参数代表多少秒之后执行
+        $this->delay($delay);
+    }
+
+    // 定义这个任务类具体的执行逻辑
+    // 当队列处理器从队列中取出任务时，会调用handle() 方法
+    public function handle()
+    {
+        // 判断对应的订单是否已支付
+        // 如果已支付则不需要关闭订单，直接退出
+        if ($this->order->paid_at) return;
+
+        // 开启事务
+        \DB::transaction(function () {
+            // 将订单的 closed 字段标记为 true, 即关闭订单
+            $this->order->update(['closed' => true]);
+            // 循环遍历订单中的商品 SKU，将订单中的数量加回到 SKU 的库存中去
+            foreach ($this->order->items as $item) {
+                $item->productSku->addStock($item->amount);
+                // 当前订单类型是秒杀订单，并且对应商品是上架且尚未到截止时间
+                if ($item->order->type === Order::TYPE_SECKILL
+                    && $item->product->on_sale
+                    && !$item->product->seckill->is_after_end) {
+                    // 将Redis中的库存 + 1
+                    \Redis::incr('seckill_sku_' . $item->productSku->id);
+                }
+            }
+
+            if ($this->order->couponCode) {
+                $this->order->couponCode->changeUsed(false);
+            }
+        });
+    }
+}
