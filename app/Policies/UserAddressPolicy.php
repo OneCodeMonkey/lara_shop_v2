2d8ced462761027076b35efb7e94c27216831cc9
OneCodeMonkey <1460018362@qq.com>
2019-04-03 10:12:51 +0000

    add policy: user address policy

---
+++ app/Policies/UserAddressPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\UserAddress;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    public function own(User $user, UserAddress $address)
+    {
+        return $address->user_id == $user->id;
+    }
+}
