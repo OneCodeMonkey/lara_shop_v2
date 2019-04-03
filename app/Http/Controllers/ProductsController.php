aa07c5d598f9b8ec0c6656005c740b2897c80b8c
OneCodeMonkey <1460018362@qq.com>
2019-04-03 08:12:23 +0000

    add controller: products controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -81,6 +81,82 @@ class ProductsController extends Controller
         }
 
         // 通过 collect() 将返回结果转为集合，并通过集合的 pluck() 取到返回的商品 ID 数组
-        $productIds = collect
+        $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
+        $products = Product::query()->byIds($productIds)->get();
+
+        // 返回一个 LengthAwarePaginator 对象
+        $pager = new LengthAwarePaginator($products, $result['hits']['total'], $perPage, $page, [
+            'path' => route('products.index', false), // 手动构建分页的 url
+        ]);
+
+        return view('products.index', [
+            'products' => $pager,
+            'filters' => [
+                'search' => $search,
+                'order' => $order,
+            ],
+            'category' => $category ?? null,
+            'products' => $properties,
+            'propertyFilters' => $propertyFilters,
+        ]);
+    }
+
+    public function show(Product $product, Request $request, ProductService $service)
+    {
+        // 判断商品是否已上架
+        if (!$product->on_sale) {
+            throw new InvalidRequestException('商品未上架');
+        }
+        $favored = false;
+        // 用户未登录时返回是null，已登录时返回是对应的用户对象
+        if ($user = $request->user()) {
+            // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
+            // boolval() 转布尔型
+            $favored = boolval($user->favoriteProducts()->find($product->id));
+        }
+
+        $reviews = OrderItem::query()->with(['order.user', 'productSku'])
+            ->where('product_id', $product->id)
+            ->whereNotNull('reviewed_at') // 筛选出已评价的
+            ->orderBy('reviewed_at', 'desc')
+            ->limit(10)
+            ->get();
+
+        $similarProductIds = $service->getSimilarProductIds($product, 4);
+        $similarProducts = Product::query()->byIds($similarProductIds)->get();
+
+        return view('products.show', [
+            'product' => $product,
+            'favored' => $favored,
+            'reviews' => $reviews,
+            'similar' => $similarProducts,
+        ]);
+    }
+
+    public function favor(Product $product, Request $request)
+    {
+        $user = $request->user();
+        if ($user->favoriteProducts()->find($product->id)) {
+            return [];
+        }
+
+        $user->favoriteProducts()->attach($product);
+
+        return [];
+    }
+
+    public function disfavor(Product $product, Request $request)
+    {
+        $user = $request->user();
+        $user->favoriteProducts()->detach($product);
+
+        return [];
+    }
+
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+
+        return view('products.favorites', ['products' => $products]);
     }
 }
