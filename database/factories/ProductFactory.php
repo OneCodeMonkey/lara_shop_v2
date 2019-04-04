2949d7f9b1bcb3ccb47cc6ea0ac2637c447fc3e2
OneCodeMonkey <1460018362@qq.com>
2019-04-04 15:54:43 +0000

    add factory: product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Product::class, function (Faker $faker) {
+    $image = $faker->randomElement([
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/r3BNRe4zXG.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/C0bVuKB2nt.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/82Wf2sg8gM.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/nIvBAQO5Pj.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/XrtIwzrxj7.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/uYEHCJ1oRp.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
+        "https://iocaffcdn.phphub.org/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
+    ]);
+
+    // 从数据库中随机取一个类目
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
+
+    return [
+        'title' => $faker->word,
+        'long_title' => $faker->sentence,
+        'description' => $faker->sentence,
+        'image' => $image,
+        'on_sale' => true,
+        'rating' => $faker->numberBetween(0, 5),
+        'sold_count' => 0,
+        'review_count' => 0,
+        'price' => 0,
+        // 将取出的类目 ID 赋给 category_id 字段
+        // 如果数据库中没有类目（$category == null)，则 category_id 设为 null
+        'category_id' => $category ? $category->id : null,
+    ];
+});
