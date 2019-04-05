e6d04eaf73cdc77131c0e994dfced6d37c4e645e
OneCodeMonkey <1460018362@qq.com>
2019-04-05 14:07:28 +0000

    add migration: create order items table

---
+++ database/migrations/2019_04_04_171221_create_order_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->unsignedInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->unsignedInteger('amount');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('rating')->nullable();
+            $table->text('review')->nullable();
+            $table->timestamps('reviewed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_items');
+    }
+}
