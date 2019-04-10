282053c33ec8aea6e2ba5ca2504de4ff7ad35be1
OneCodeMonkey <1460018362@qq.com>
2019-04-10 20:04:40 +0000

    [editing]add blade layout: autu/password/reset

---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    {{ __('Reset Password') }}
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
