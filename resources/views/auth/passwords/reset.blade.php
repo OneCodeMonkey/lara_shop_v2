9c8f0e16383fd4afe7f63a48c13d3ae7d6a3141c
OneCodeMonkey <1460018362@qq.com>
2019-04-11 15:53:21 +0000

    修改三元表达式写法，空格留在 {{ . }} 的表达式之内

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -15,7 +15,7 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control {{ $error->has('email') ? 'is-invalid' : '' }}}" name="email" value="{{ $email ?? old('email') }}" required autofocus />
+                                <input id="email" type="email" class="form-control{{ $error->has('email') ? ' is-invalid' : '' }}}" name="email" value="{{ $email ?? old('email') }}" required autofocus />
                                 @if($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -27,7 +27,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" name="password" required />
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required />
                                 @if($errors->has('password'))
                                     <span classs="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
