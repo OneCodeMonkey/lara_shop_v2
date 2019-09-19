8e565a7be8018fdd1cb05e516f856209ea215fe3
OneCodeMonkey <1460018362@qq.com>
2019-09-19 14:56:01 +0800

    add default value

--- config/database.php
+++ config/database.php
@@ -130,6 +130,6 @@ return [
 
     'elasticsearch' => [
         // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
-        'hosts' => explode(',', env('ES_HOSTS')),
+        'hosts' => explode(',', env('ES_HOSTS', 'localhost')),
     ]
 ];
