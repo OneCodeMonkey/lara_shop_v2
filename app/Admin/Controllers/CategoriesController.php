e98cf4a81986c1f4b2e6fc617d84fd6b71b436f5
OneCodeMonkey <1460018362@qq.com>
2019-09-23 11:56:24 +0800

    fix

--- app/Admin/Controllers/CategoriesController.php
+++ app/Admin/Controllers/CategoriesController.php
@@ -4,7 +4,7 @@ namespace App\Admin\Controllers;
 
 use App\Models\Category;
 use App\Http\Controllers\Controller;
-use Encore\Admin\Controller\HasResourceActions;
+use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
