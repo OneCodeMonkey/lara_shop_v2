5ea3781f728de96def1448df605e77abf42f0d88
OneCodeMonkey <1460018362@qq.com>
2019-09-23 11:38:49 +0800

    fix

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -17,7 +17,7 @@ class ProductsSeeder extends Seeder
             // 每个 product 创建 3 个 SKU，且每个 SKU 的 'product_id' 字段都设为当前商品 id
             $skus = factory(\App\Models\ProductSku::class, 3)->create(['product_id' => $product->id]);
             // 找出价格最低的 SKU 价格，并把商品价格设为此最低价
-            $products->update(['price' => $skus->min('price')]);
+            $product->update(['price' => $skus->min('price')]);
         }
     }
 }
