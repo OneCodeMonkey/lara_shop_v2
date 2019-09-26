ee7c301284ac39f549a34c965538a66b0f56afc3
OneCodeMonkey <1460018362@qq.com>
2019-09-26 15:27:35 +0800

    modify

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由<a href="http://liuyang1.com" target="_blank">OneCodeMonkey</a>设计和编码<span style="color: #e27575;font-size: 14px;">❤</span>
+            由<a href="http://github.com/OneCodeMonkey" target="_blank">OneCodeMonkey</a>设计和编码<span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
         <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
     </div>
